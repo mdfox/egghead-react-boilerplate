6bd8570d369c0bd45443b76415617e1b7ae4f8b1
Matthew D Fox <mdfox72@gmail.com>
2018-12-14 13:22:16 -0600

    Adds styles and react-hot-loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {hot} from 'react-hot-loader'
 
 class App extends React.Component {
+    state = {
+        count: 0
+    }
+
     render() {
-        return <h1>Hello world</h1>
+        return (
+            <div>
+                <h1>Hello world</h1>
+                <h2 className={this.state.count > 10 ? 'warning' : null}>
+                    Count: {this.state.count}
+                </h2>
+                <button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
+                <button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
+            </div>
+        )
     }
 }
 
-export default App
+export default hot(module)(App)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,7 +2,6 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.join(__dirname, 'dist'),
@@ -15,8 +14,17 @@ module.exports = {
                 loader: 'babel-loader',
                 exclude: /node_modules/,
                 options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                    plugins: [
+                        '@babel/plugin-proposal-class-properties',
+                        'react-hot-loader/babel'
+                    ]
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: /node_modules/
             }
         ]
     },
