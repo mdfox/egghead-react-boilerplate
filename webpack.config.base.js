8ec0f1a633674a33c63248e96836a99adbf88d54
Matthew D Fox <mdfox72@gmail.com>
2018-12-21 09:00:29 -0600

    Adds incremental loading. Adds Warning.js to be incrementally loaded

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -26,6 +26,7 @@ module.exports = {
                     '@babel/preset-react'],
                     plugins: [
                         '@babel/plugin-proposal-class-properties',
+                        '@babel/plugin-syntax-dynamic-import',
                         'react-hot-loader/babel'
                     ]
                 }
